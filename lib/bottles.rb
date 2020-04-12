f82d1dcdf9d4df9dee64c1d6d6675a244d2f6e91
Tim Austin <tim@neenjaw.com>
2020-04-12 17:43:08 -0600

    speculatively-general

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,57 +1,63 @@
 class Bottles
-  def verse(n)
-    case n
-    when 2
-      verse_2()
-    when 1
-      verse_1()
-    when 0
-      verse_0()
-    else
-      verse_n(n)
-    end
+  NoMore = lambda do |verse|
+    "No more bottles of beer on the wall, " +
+    "no more bottles of beer.\n" +
+    "Go to the store and buy some more, " +
+    "99 bottles of beer on the wall.\n"
   end
 
-  def verses(start, finish)
-    (finish..start)
-      .to_a
-      .reverse
-      .map { |n| verse(n) }
-      .join("\n")
+  LastOne = lambda do |verse|
+    "1 bottle of beer on the wall, " +
+    "1 bottle of beer.\n" +
+    "Take it down and pass it around, " +
+    "no more bottles of beer on the wall.\n"
+  end
+
+  Penultimate = lambda do |verse|
+    "2 bottles of beer on the wall, " +
+    "2 bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "1 bottle of beer on the wall.\n"
+  end
+
+  Default = lambda do |verse|
+    "#{verse.number} bottles of beer on the wall, " +
+    "#{verse.number} bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "#{verse.number - 1} bottles of beer on the wall.\n"
   end
 
   def song
     verses(99, 0)
   end
 
-  private
-
-  def verse_n(n)
-    <<~VERSE
-      #{n} bottles of beer on the wall, #{n} bottles of beer.
-      Take one down and pass it around, #{n-1} bottles of beer on the wall.
-    VERSE
+  def verses(finish, start)
+    (finish).downto(start).map {|verse_number|
+      verse(verse_number) }.join("\n")
   end
 
-  def verse_2
-    <<~VERSE
-      2 bottles of beer on the wall, 2 bottles of beer.
-      Take one down and pass it around, 1 bottle of beer on the wall.
-    VERSE
+  def verse(number)
+    verse_for(number).text
   end
 
-  def verse_1
-    <<~VERSE
-      1 bottle of beer on the wall, 1 bottle of beer.
-      Take it down and pass it around, no more bottles of beer on the wall.
-    VERSE
+  def verse_for(number)
+    case number
+    when 0 then Verse.new(number, &NoMore)
+    when 1 then Verse.new(number, &LastOne)
+    when 2 then Verse.new(number, &Penultimate)
+    else        Verse.new(number, &Default)
+    end
+  end
+end
+
+class Verse
+  attr_reader :number
+  def initialize(number, &lyrics)
+    @number = number
+    @lyrics = lyrics
   end
 
-  def verse_0
-    <<~VERSE
-      No more bottles of beer on the wall, no more bottles of beer.
-      Go to the store and buy some more, 99 bottles of beer on the wall.
-    VERSE
+  def text
+    @lyrics.call self
   end
-
 end
