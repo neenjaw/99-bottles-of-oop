8df9ee064fae35c2c0b617a48d28f4708ce640a2
Tim Austin <tim@neenjaw.com>
2020-04-13 10:17:17 -0600

    concretely abstract

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,57 +1,88 @@
 class Bottles
-  def verse(n)
-    case n
-    when 2
-      verse_2()
-    when 1
-      verse_1()
-    when 0
-      verse_0()
-    else
-      verse_n(n)
-    end
-  end
-
-  def verses(start, finish)
-    (finish..start)
-      .to_a
-      .reverse
-      .map { |n| verse(n) }
-      .join("\n")
-  end
 
   def song
     verses(99, 0)
   end
 
-  private
-
-  def verse_n(n)
-    <<~VERSE
-      #{n} bottles of beer on the wall, #{n} bottles of beer.
-      Take one down and pass it around, #{n-1} bottles of beer on the wall.
-    VERSE
+  def verses(bottles_at_start, bottles_at_end)
+    bottles_at_start.downto(bottles_at_end).map do |bottles|
+      verse(bottles)
+    end.join("\n")
   end
 
-  def verse_2
-    <<~VERSE
-      2 bottles of beer on the wall, 2 bottles of beer.
-      Take one down and pass it around, 1 bottle of beer on the wall.
-    VERSE
+  def verse(bottles)
+    Round.new(bottles).to_s
+  end
+end
+
+class Round
+  attr_reader :bottles
+  def initialize(bottles)
+    @bottles = bottles
   end
 
-  def verse_1
-    <<~VERSE
-      1 bottle of beer on the wall, 1 bottle of beer.
-      Take it down and pass it around, no more bottles of beer on the wall.
-    VERSE
+  def to_s
+    challenge + response
   end
 
-  def verse_0
-    <<~VERSE
-      No more bottles of beer on the wall, no more bottles of beer.
-      Go to the store and buy some more, 99 bottles of beer on the wall.
-    VERSE
+  def challenge
+    bottles_of_beer.capitalize + " " + on_wall + ", " +
+    bottles_of_beer + ".\n"
   end
 
+  def response
+    go_to_the_store_or_take_one_down + ", " +
+    bottles_of_beer + " " + on_wall + ".\n"
+  end
+
+  def bottles_of_beer
+    "#{anglicized_bottle_count} #{pluralized_bottle_form} of #{beer}"
+  end
+
+  def beer
+    "beer"
+  end
+
+  def on_wall
+    "on the wall"
+  end
+
+  def pluralized_bottle_form
+    last_beer? ? "bottle" : "bottles"
+  end
+
+  def anglicized_bottle_count
+    all_out? ? "no more" : bottles.to_s
+  end
+
+  def go_to_the_store_or_take_one_down
+    if all_out?
+      @bottles = 99
+      buy_new_beer
+    else
+      lyrics = drink_beer
+      @bottles -= 1
+      lyrics
+    end
+  end
+
+  def buy_new_beer
+    "Go to the store and buy some more"
+  end
+
+  def drink_beer
+    "Take #{it_or_one} down and pass it around"
+  end
+
+  def it_or_one
+    last_beer? ? "it" : "one"
+  end
+
+  def all_out?
+    bottles.zero?
+  end
+
+  def last_beer?
+    bottles == 1
+  end
 end
